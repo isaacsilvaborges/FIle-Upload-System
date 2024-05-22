d7d7bd2638989a7970beaf925bda55a5737cbaa3
Raquel <raquelparadella@gmail.com>
2024-05-22 16:52:54 -0300

    Mudanças barradas por permissão de AWS

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import React, { Component } from 'react';
+import './App.css'; //Imports the style of App
+import React, { Component } from 'react'; //Componet is a block from React
 
-class App extends Component {
+class App extends Component { //Creation of the app
 
-  state = {
+  state = { //Boolean state
     selectedFile: null,
     fileUploadedSuccessfully: false
   }
 
-  onFileChange = event => {
+  onFileChange = event => { //Extracts the event
     this.setState({selectedFile: event.target.files[0]})
   }
 
-  onFileUpload = () => {
+  onFileUpload = () => { //Holds the state and informations regarding the file
     const formData = new FormData();
     formData.append(
       "demo file",
@@ -20,25 +20,47 @@ class App extends Component {
       this.state.selectedFile.name
     )
 
-    //call api, like rebooting the application
+    //call api, like rebooting the application and printing information
     console.log(formData);
     this.setState({selectedFile: null});
     this.setState({fileUploadedSuccessfully: true})
   }
 
-  fileData = () => {
+  fileData = () => { //Extracts everything from the file and uploads
     if (this.state.selectedFile) {
-      <div>
-        <h2>File Details:</h2>
-        <p>FIle Name: {this.state.selectedFile.name}</p>
-        <p>FIle type: {this.state.selectedFile.type}</p>
-      </div>
+      const file = this.state.selectedFile;
+      const lastModifiedDate = new Date(file.lastModified);
+      return (
+        <div>
+          <h2>File Details:</h2>
+          <p>File Name: {file.name}</p>
+          <p>File Type: {file.type}</p>
+          <p>
+            Last Modified:{" "}
+            {lastModifiedDate.toDateString()}
+          </p>
+        </div>
+      );
+    } else if (this.state.fileUploadedSuccessfully) {
+        return (
+          <div>
+            <br />
+            <h4>Your file has been succesfully uploaded.</h4>
+          </div>
+        );
+    } else {
+        return (
+          <div>
+            <br />
+            <h4>Choose a file and then press the Upload button</h4>
+          </div>
+        );
     }
   }
 
-  render() {
+  render() { //Creates the screen with the functions
     return (
-      <div>
+      <div className="container">
         <h2>Jinmeister File Upload System</h2>
         <h3>File Upload with React and Serverless API!</h3>
         <div>
@@ -49,7 +71,7 @@ class App extends Component {
         </div>
         {this.fileData()}
       </div>
-    )
+    );
   }
 }
 export default App;
